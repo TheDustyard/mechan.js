ad896a12de98e6619190751da508da19bd1220e9
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-07-29 17:52:08 -0400

    in:b:ex.ts

--- mechan.js/index.ts
+++ mechan.js/index.ts
@@ -5,13 +5,20 @@ export const Discord = dcord;
 
 export * from './Command/Parameters/CommandParameters';
 export * from './Command/Parameters/ParameterType';
+
 export * from './Command/Permissions/PermissionCheck';
+
 export * from './Command/Command';
 export * from './Command/CommandBuilder';
-export * from './Command/CommandGroup';
-
 export * from './Command/CommandContext';
+export * from './Command/CommandErrorContext';
+export * from './Command/CommandGroup';
+export * from './Command/CommandMap';
+export * from './Command/CommandParser';
+
+
 export * from './CommandHandler/CommandHandler';
 export * from './CommandHandler/CommandHandlerConfig';
+
 
 export * from './Help/HelpMode';
