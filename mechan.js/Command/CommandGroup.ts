558f1558fadcc91d26bc3e2ac74f43b691173c78
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-08-03 15:16:12 -0400

    parse

--- mechan.js/Command/CommandGroup.ts
+++ mechan.js/Command/CommandGroup.ts
@@ -2,7 +2,8 @@
     Command,
     CommandBuilder,
     CommandHandler,
-    PermissionCheck
+    PermissionCheck,
+    CommandContext
 } from '../';
 
 export class CommandGroup {
@@ -17,11 +18,15 @@ export class CommandGroup {
     /**
      * Subcommands
      */
-    public commands: Command[];
+    public commands: Map<string, Command>;
     /**
-     * Subgroup
+     * Subgroups
      */
-    public groups: CommandGroup[];
+    public groups: Map<string, CommandGroup>;
+    /**
+     * Parent group
+     */
+    public parent: CommandGroup;
     /**
      * Checks to preform on all commands
      */
@@ -37,17 +42,110 @@ export class CommandGroup {
 
     /**
      * Create a command group
+     * @param handler - Handler that handles the command
+     * @param parent - Parent group
      * @param name - Name of the group
      * @param commands - Subcommands
+     * @param prechecks - Checks to preform on all commands
+     * @param category - Category the command fits into
+     * @param visible - Whether or not the command is visible in the default help menu
      */
-    constructor(handler: CommandHandler, name: string, commands: Command[] = [], prechecks: PermissionCheck[] = [], category: string = null, visible: boolean = true) {
+    constructor(handler: CommandHandler, parent: CommandGroup, name: string, commands: Command[] = [], prechecks: PermissionCheck[] = [], category: string = null, visible: boolean = true) {
         this.handler = handler;
-        this.commands = commands;
+        this.parent = parent;
+        this.commands = new Map<string, Command>();
+        commands.forEach((v, index, array) => {
+            this.commands.set(v.name, v);
+        })
         this.name = name;
         this.prechecks = prechecks;
         this.category = category;
         this.visible = visible;
-        this.groups = [];
+        this.groups = new Map<string, CommandGroup>();
+    }
+
+    /**
+     * Get an item from the map
+     * @param index - Index to start looking at the parts
+     * @param parts - Parts of the item
+     */
+    public getItem(index: number = 0, parts: string[]): CommandGroup {
+        if (index != parts.length) {
+            let nextPart: string = parts[index];
+            let nextGroup = this.groups.get(nextPart.toLowerCase())
+            if (nextGroup) {
+                return nextGroup.getItem(index + 1, parts);
+            } else {
+                return null;
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Get the comamnds in this map
+     */
+    public getCommands(): Command[] {
+        if (Array.from(this.commands.values()).length > 0)
+            return Array.from(this.commands.values());
+        else if (this.parent != null)
+            return this.parent.getCommands();
+        else
+            return null;
+    }
+
+    /**
+     * Get commands from the given string
+     * @param text
+     */
+    public getCommandsFromString(text: string): Command[] {
+        return this.getCommandsFromIndex(0, text.split(' '));
+    }
+    /**
+     * Get command at index
+     * @param index - Index of the command
+     * @param parts - Parts of the command
+     */
+    public getCommandsFromIndex(index: number, parts: string[]): Command[] {
+        if (index != parts.length) {
+            let nextPart: string = parts[index];
+            let nextGroup: CommandGroup;
+            try {
+                nextGroup = this.groups.get(nextPart.toLowerCase());
+                var cmd = nextGroup.getCommandsFromIndex(index + 1, parts);
+                if (cmd != null)
+                    return cmd;
+            } catch (e) { }
+        }
+
+        if (this.commands != null)
+            return Array.from(this.commands.values());
+        return null;
+    }
+
+    /**
+    * Check if the module can run
+    * @param context - Command context
+    */
+    public canRun(context: CommandContext): [boolean, string] {
+        let error: string = null;
+        if (Array.from(this.commands.values()).length > 0) {
+            for (let cmd of Array.from(this.commands.values())) {
+                let [can, err] = cmd.canRun(context);
+                error = err;
+                if (can)
+                    return [true, null];
+            }
+        }
+        if (this.groups.size > 0) {
+            for (let item of this.groups) {
+                let [can, err] = item[1].canRun(context);
+                error = err;
+                if (can)
+                    return [true, null];
+            }
+        }
+        return [false, error];
     }
 
 }
@@ -57,12 +155,13 @@ export class CommandGroupBuilder extends CommandGroup {
     /**
      * Create a group builder
      * @param handler - Command handler to regester to
+     * @param parent - Parent group
      * @param name - Name of the group
      * @param category - Category of the group
      * @param prechecks - Prechecks to run
      */
-    constructor(handler: CommandHandler, name: string = "", category: string = null, prechecks: PermissionCheck[] = []) {
-        super(handler, name, [], prechecks, category, true);
+    constructor(handler: CommandHandler, parent: CommandGroup, name: string = "", category: string = null, prechecks: PermissionCheck[] = []) {
+        super(handler, parent, name, [], prechecks, category, true);
     }
 
     /**
@@ -103,9 +202,11 @@ export class CommandGroupBuilder extends CommandGroup {
      * Create subGroup
      * @param name - Group name
      */
-    public createGroup(name: string): CommandGroupBuilder {
-        let builder = new CommandGroupBuilder(this.handler, this.name + ' ' + name, this.category, this.prechecks);
-        this.groups.push(builder);
+    public createGroup(name: string, callback?: (builder: CommandGroupBuilder) => void): CommandGroupBuilder {
+        let builder = new CommandGroupBuilder(this.handler, this, this.name + ' ' + name, this.category, this.prechecks);
+        if (callback)
+            callback(builder);
+        this.groups.set(name, builder);
         return builder;
     }
 
---
+++ mechan.js/Command/CommandParser.ts
@@ -0,0 +1,88 @@
+﻿import {
+    CommandGroup,
+    Command
+} from "../";
+
+enum ParserPart {
+    None,
+    Parameter,
+    QuotedParameter,
+    DoubleQuotedParameter
+}
+
+export class ParsedCommandInfo {
+    /**
+     * If the command parsing succeeded
+     */
+    wasSuccess: boolean;
+    /**
+     * Commands parsed out
+     */
+    commands: Command[];
+    /**
+     * End position
+     */
+    endPos: number;
+
+    /**
+     * Create an instance
+     * @param wasSuccess - If the command parsing succeeded
+     * @param commands - Commands parsed out
+     * @param endPos - End position
+     */
+    constructor(wasSuccess: boolean, commands: Command[], endPos: number) {
+        this.wasSuccess = wasSuccess;
+        this.commands = commands;
+        this.endPos = endPos;
+    }
+};
+
+export class CommandParser {
+
+    public static ParseCommand(input: string, group: CommandGroup): ParsedCommandInfo {
+        let startPosition: number = 0;
+        let endPosition: number = 0;
+        let inputLength: number = input.length;
+        let isEscaped: boolean = false;
+        let commands: Command[] = null;
+        let endPos: number = 0;
+
+        if (input == "")
+            return new ParsedCommandInfo(false, commands, endPos);
+
+        while (endPosition < inputLength) {
+            let currentChar: string = input[endPosition++];
+            if (isEscaped)
+                isEscaped = false;
+            else if (currentChar == '\\')
+                isEscaped = true;
+            let isWhitespace: boolean = CommandParser.IsWhiteSpace(currentChar);
+
+            if ((!isEscaped && isWhitespace) || endPosition >= inputLength) {
+                let length: number = (isWhitespace ? endPosition - 1 : endPosition) - startPosition;
+                let temp: string = input.substring(startPosition, length);
+                if (temp == "")
+                    startPosition = endPosition;
+                else {
+                    var newGroup = group.getItem(0, temp.split(' '));
+                    if (newGroup != null) {
+                        group = newGroup;
+                        endPos = endPosition;
+                    }
+                    else
+                        break;
+                    startPosition = endPosition;
+                }
+            }
+        }
+        commands = group.getCommands();
+    }
+
+    /**
+     * Check if c is whitespace
+     * @param c - Character to check
+     */
+    private static IsWhiteSpace(c: string): boolean {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+    };
+}
